99642e0ce37af445adb5ff86aa069d534ca7ba0f
sokoeurn88 <sokoeurn.chhaya@gmail.com>
2024-08-09 11:00:54 -0500

    ArrayList and Array

---
+++ src/day02/ArrayList_Demo.java
@@ -0,0 +1,25 @@
+package day02;
+
+import java.util.ArrayList;
+
+public class ArrayList_Demo {
+	
+	public static void main(String[] args) {
+		
+		String[] names = {"sokoeurn", "chhay"};
+		System.out.println(names[0]);
+		System.out.println(names[1]);
+		
+		ArrayList<String> givenNames = new ArrayList<String>();
+		givenNames.add("sokoeurn");
+		givenNames.add("sarom");
+		System.out.println(givenNames);
+		
+		
+		
+		
+		
+	}
+	
+
+}
